9298b860f421b1bb1403b30861b325d5230b4401
Joe02 <Joe02araujo@gmail.com>
2020-06-02 10:30:35 -0300

    Created back button in case the user wants to redo a question.

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,8 @@ class MyAppState extends State<MyApp> {
 
   var questionNumber = 1;
 
+  var _isBackButtonVisible = false;
+
   @override
   Widget build(BuildContext context) {
 
@@ -41,37 +43,67 @@ class MyAppState extends State<MyApp> {
           title: Text("Pergunta #$questionNumber"),
         ),
         body: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Center(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(shownQuestion.question, style: TextStyle(
-                      fontSize: 21, fontWeight: FontWeight.bold)),
+                      fontSize: 21, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
                 )),
             Flexible(
-              child: ListView(
-                  shrinkWrap: true,
-                  padding: const EdgeInsets.only(left: 40, right: 40, top: 10),
-                  children: new List.generate(
-                      shownQuestion.answers.length,
-                          (index) =>
-                          RaisedButton(
-                            child: Text(shownQuestion.answers[index]),
-                            onPressed: () {
-                              setState(() {
-                                answers.add(shownQuestion.answers[index]);
-                                if (shownQuestion == firstQuestion) {
-                                  shownQuestion = secondQuesion;
-                                } else if (shownQuestion == secondQuesion){
-                                  shownQuestion = thirdQuestion;
-                                } else {
-                                  //Add finished quiz screen.
-                                }
-                              });
-                            },
-                          )
-                  )
+              child: Container(
+                margin: EdgeInsets.only(bottom: 25),
+                child: ListView(
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.only(left: 40, right: 40, top: 10),
+                    children: new List.generate(
+                        shownQuestion.answers.length,
+                            (index) =>
+                            RaisedButton(
+                              child: Text(shownQuestion.answers[index], style: TextStyle(fontSize: 18),),
+                              onPressed: () {
+                                setState(() {
+                                  answers.add(shownQuestion.answers[index]);
+                                  if (shownQuestion == firstQuestion) {
+                                    _isBackButtonVisible = true;
+                                    shownQuestion = secondQuesion;
+                                    questionNumber = 2;
+                                  } else if (shownQuestion == secondQuesion){
+                                    _isBackButtonVisible = true;
+                                    shownQuestion = thirdQuestion;
+                                    questionNumber = 3;
+                                  } else {
+                                    //Add finished quiz screen.
+                                  }
+                                });
+                              },
+                            )
+                    )
+                ),
+              ),
+            ),
+            Visibility(
+              visible: _isBackButtonVisible,
+              child: RaisedButton(
+                child: Text("Voltar para última pergunta"),
+                onPressed: () {
+                  setState(() {
+                    if (shownQuestion == secondQuesion) {
+                      answers.removeLast();
+                      _isBackButtonVisible = false;
+                      shownQuestion = firstQuestion;
+                      questionNumber = 1;
+                    } else {
+                      answers.removeLast();
+                      _isBackButtonVisible = true;
+                      shownQuestion = secondQuesion;
+                      questionNumber = 2;
+                    }
+                  });
+                },
               ),
             )
           ],
